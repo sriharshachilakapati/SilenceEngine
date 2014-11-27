94474b218c495b57e7251c5bc66068d24efccfc1
Sri Harsha Chilakapati <sriharshachilakapati@gmail.com>
2014-11-27 20:27:01 +0530

    Switch to Gradle

---
+++ src/main/java/com/shc/silenceengine/core/Display.java
@@ -0,0 +1,429 @@
+package com.shc.silenceengine.core;
+
+import com.shc.silenceengine.graphics.Color;
+import com.shc.silenceengine.graphics.Shader;
+import com.shc.silenceengine.graphics.Texture;
+import com.shc.silenceengine.input.Keyboard;
+import org.lwjgl.opengl.GLContext;
+import org.lwjgl.system.glfw.ErrorCallback;
+import org.lwjgl.system.glfw.GLFWvidmode;
+import org.lwjgl.system.glfw.WindowCallback;
+import org.lwjgl.system.glfw.WindowCallbackAdapter;
+
+import java.nio.ByteBuffer;
+
+import static org.lwjgl.opengl.GL11.*;
+import static org.lwjgl.system.glfw.GLFW.*;
+import static org.lwjgl.system.MemoryUtil.*;
+
+/**
+ * The Display class takes care of creating a display and managing it.
+ * As a user of SilenceEngine, I don't expect you to create the Display,
+ * the display creation will be handled automatically by the Game class.
+ *
+ * @author Sri Harsha Chilakapati
+ */
+public final class Display
+{
+    // The display handle
+    private static long displayHandle   = NULL;
+
+    // Width and height of the display
+    private static int width         = 640;
+    private static int height        = 480;
+
+    // Width and height of windowed display, used to
+    // restore the properties to the newly created one
+    private static int oldWidth      = 640;
+    private static int oldHeight     = 480;
+
+    // Position of the windowed display, restored when
+    // fullscreen is switched off
+    private static int oldPosX       = 0;
+    private static int oldPosY       = 0;
+
+    // The position of the display in screen coordinates
+    private static int posX          = 0;
+    private static int posY          = 0;
+
+    // The title of the Display
+    private static String title         = "SilenceEngine";
+
+    // Private flags to maintain the Display
+    private static boolean resized    = false;
+    private static boolean fullScreen = false;
+
+    // Clear color
+    private static Color clearColor = Color.BLACK;
+
+    // Mouse Coordinates
+    public static int mouseX;
+    public static int mouseY;
+
+    /** Private constructor. Prevent instantiation */
+    private Display()
+    {
+    }
+
+    /**
+     * A private method to handle the creation of GLFW windows. Takes care of creating
+     * the window with windowing hints, a size, a title, fullscreen or not, parent window
+     * to share the context, and whether initially visible or not.
+     *
+     * @param width    The width of the window
+     * @param height   The height of the window
+     * @param title    The title of the window
+     * @param monitor  The monitor to create the window on
+     * @param parent   The parent window, if the context needs to be shared
+     * @param visible  Is the window visible upon creation?
+     *
+     * @return A window handle. (GLFWWindow* as in C++, but this is Java, so a long)
+     */
+    private static long createWindow(int width, int height, String title, long monitor, long parent, boolean visible)
+    {
+        // Window Hints for OpenGL context
+        glfwWindowHint(GLFW_SAMPLES, 4);
+        glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+        glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 2);
+        glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+        glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+        glfwWindowHint(GLFW_VISIBLE, visible ? GL_TRUE : GL_FALSE);
+
+        // Create the window
+        long window = glfwCreateWindow(width, height, title, monitor, parent);
+
+        if (window == NULL)
+            throw new SilenceException("Error creating Display. Your system is unsupported.");
+
+        // Take care of OpenGL context
+        glfwMakeContextCurrent(window);
+        glfwSwapInterval(1);
+
+        GLContext.createFromCurrent();
+
+        // Initialize OpenGL
+        glEnable(GL_BLEND);
+        glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+
+        glEnable(GL_DEPTH_TEST);
+
+        if (Shader.DEFAULT == null)
+            Shader.loadDefaultShader();
+        if (Texture.EMPTY == null)
+            Texture.loadNullTexture();
+
+        Shader.DEFAULT.use();
+        Texture.EMPTY.bind();
+
+        // Window callbacks
+        WindowCallback.set(window, new WindowCallbackAdapter()
+        {
+            @Override
+            public void windowSize(long window, int width, int height)
+            {
+                Display.width  = width;
+                Display.height = height;
+
+                resized = true;
+            }
+
+            @Override
+            public void key(long window, int key, int scanCode, int action, int mods)
+            {
+                Keyboard.setKey(key, action == GLFW_PRESS);
+            }
+
+            @Override
+            public void windowPos(long window, int xPos, int yPos)
+            {
+                Display.posX = xPos;
+                Display.posY = yPos;
+            }
+
+            @Override
+            public void cursorPos(long window, double xPos, double yPos)
+            {
+                mouseX = (int) xPos;
+                mouseY = (int) yPos;
+            }
+        });
+
+        return window;
+    }
+
+    /**
+     * Creates the default display, in windowed mode, initially hidden.
+     */
+    public static void create()
+    {
+        glfwSetErrorCallback(ErrorCallback.Util.getDefault());
+
+        // Initialize GLFW
+        if (glfwInit() != GL_TRUE)
+            throw new SilenceException("Error creating Display. Your system is unsupported.");
+
+        // Create the window
+        displayHandle = createWindow(width, height, title, NULL, NULL, false);
+        centerOnScreen();
+    }
+
+    /**
+     * Makes the display visible to the users.
+     */
+    public static void show()
+    {
+        glfwShowWindow(displayHandle);
+    }
+
+    /**
+     * @return true if the user has clicked the close button, or pressed
+     *         platform specific close shortcut keys like Cmd-Q(Mac) or
+     *         Alt-F4 (linux &amp; windows)
+     */
+    public static boolean isCloseRequested()
+    {
+        return glfwWindowShouldClose(displayHandle) == GL_TRUE;
+    }
+
+    /**
+     * Sets the viewport of the display.
+     *
+     * @param x      The x-coordinate of the top-left point
+     * @param y      The y-coordinate of the top-left point
+     * @param width  The width of the window
+     * @param height The height of the window
+     */
+    public static void setViewport(int x, int y, int width, int height)
+    {
+        glViewport(x, y, width, height);
+    }
+
+    /**
+     * Hides the display from being shown to the user.
+     */
+    public static void hide()
+    {
+        glfwHideWindow(displayHandle);
+    }
+
+    /**
+     * Destroy the display, will be unusable until create is called.
+     * Please don't call this method in between frames, unless you
+     * are keenly awaiting for a number of runtime exceptions to be
+     * thrown.
+     */
+    public static void destroy()
+    {
+        Shader.DEFAULT.dispose();
+        Texture.EMPTY.dispose();
+
+        glfwDestroyWindow(displayHandle);
+        glfwTerminate();
+    }
+
+    /**
+     * Updates the display. Swaps the buffers and polls new events.
+     */
+    public static void update()
+    {
+        glfwSwapBuffers(displayHandle);
+        glfwPollEvents();
+
+        glClearColor(clearColor.getRed(), clearColor.getGreen(), clearColor.getBlue(), clearColor.getAlpha());
+    }
+
+    /**
+     * @return the handle of the current display. Please don't store
+     *         this handle, and always use this method to get one,
+     *         because a stored handle can get invalid at any time.
+     */
+    public static long getDisplayHandle() { return displayHandle; }
+
+    /**
+     * @return the height of the display
+     */
+    public static int getHeight()
+    {
+        return height;
+    }
+
+    /**
+     * Sets the height of the display, measured in screen coordinates.
+     *
+     * @param height The height of the window
+     */
+    public static void setHeight(int height)
+    {
+        Display.height = height;
+        glfwSetWindowSize(displayHandle, width, height);
+    }
+
+    /**
+     * @return The width of the display, in screen coordinates.
+     */
+    public static int getWidth()
+    {
+        return width;
+    }
+
+    /**
+     * Sets the width of the display, in screen coordinates.
+     *
+     * @param width The width of the window.
+     */
+    public static void setWidth(int width)
+    {
+        Display.width = width;
+        glfwSetWindowSize(displayHandle, width, height);
+    }
+
+    /**
+     * @return The aspect ratio of the Display
+     */
+    public static float getAspectRatio()
+    {
+        return ((float)width)/((float)height);
+    }
+
+    /**
+     * Centers the display on screen.
+     */
+    public static void centerOnScreen()
+    {
+        if (fullScreen || displayHandle == NULL)
+            return;
+
+        ByteBuffer vidMode = glfwGetVideoMode(glfwGetPrimaryMonitor());
+        glfwSetWindowPos(displayHandle, (GLFWvidmode.width(vidMode) - width)/2,
+                                        (GLFWvidmode.height(vidMode) - height)/2);
+    }
+
+    /**
+     * Sets the window position on the screen
+     * @param x The x-coordinate of the window (in screen coordinates)
+     * @param y The y-coordinate of the window (in screen coordinates)
+     */
+    public static void setPosition(int x, int y)
+    {
+        if (fullScreen || displayHandle == NULL)
+            return;
+
+        posX = x;
+        posY = y;
+
+        glfwSetWindowPos(displayHandle, x, y);
+    }
+
+    /**
+     * Sets the clear color of the Display, i.e., the background color
+     * @param c The background color to clear the window
+     */
+    public static void setClearColor(Color c)
+    {
+        clearColor = c;
+    }
+
+    /**
+     * @return whether the Display is fullscreen or not
+     */
+    public static boolean isFullScreen()
+    {
+        return fullScreen;
+    }
+
+    /**
+     * Sets the state of fullscreen of the Display.
+     * @param fullScreen If true, window will be made fullscreen
+     */
+    public static void setFullScreen(boolean fullScreen)
+    {
+        if (Display.fullScreen == fullScreen)
+            return;
+
+        Display.fullScreen = fullScreen;
+
+        if (fullScreen)
+        {
+            ByteBuffer vidMode = glfwGetVideoMode(glfwGetPrimaryMonitor());
+
+            // Save window size
+            oldWidth = width;
+            oldHeight = height;
+
+            // Save window position
+            oldPosX = posX;
+            oldPosY = posY;
+
+            width = GLFWvidmode.width(vidMode);
+            height = GLFWvidmode.height(vidMode);
+        }
+        else
+        {
+            // Restore window size
+            width = oldWidth;
+            height = oldHeight;
+
+            // Restore window position
+            posX = oldPosX;
+            posY = oldPosY;
+        }
+
+        // Create new window
+        long fsDisplayHandle = createWindow(width, height, title, fullScreen ? glfwGetPrimaryMonitor() : NULL, displayHandle, true);
+        glfwDestroyWindow(displayHandle);
+        displayHandle = fsDisplayHandle;
+
+        setPosition(posX, posY);
+        show();
+    }
+
+    /**
+     * @return The title of the Display
+     */
+    public static String getTitle()
+    {
+        return title;
+    }
+
+    /**
+     * Sets the title of the Display
+     * @param title The title of the window
+     */
+    public static void setTitle(String title)
+    {
+        Display.title = title;
+
+        if (displayHandle != NULL)
+            glfwSetWindowTitle(displayHandle, title);
+    }
+
+    /**
+     * @return True if the display has been resized
+     */
+    public static boolean wasResized()
+    {
+        if (resized)
+        {
+            resized = false;
+            return true;
+        }
+
+        return false;
+    }
+
+    /**
+     * Hides the cursor over the Display
+     */
+    public static void hideCursor()
+    {
+        glfwSetInputMode(displayHandle, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
+    }
+
+    /**
+     * Shows the cursor over the Display
+     */
+    public static void showCursor()
+    {
+        glfwSetInputMode(displayHandle, GLFW_CURSOR, GLFW_CURSOR_NORMAL);
+    }
+}
---
+++ src/main/java/com/shc/silenceengine/graphics/Batcher.java
@@ -0,0 +1,436 @@
+package com.shc.silenceengine.graphics;
+
+import com.shc.silenceengine.math.Matrix4;
+import com.shc.silenceengine.math.Vector2;
+import com.shc.silenceengine.math.Vector3;
+import com.shc.silenceengine.math.Vector4;
+import org.lwjgl.BufferUtils;
+
+import java.nio.FloatBuffer;
+
+import static org.lwjgl.opengl.GL11.*;
+import static org.lwjgl.opengl.GL15.*;
+import static org.lwjgl.opengl.GL20.*;
+import static org.lwjgl.opengl.GL30.*;
+
+/**
+ * A simple class which eases the rendering of Graphics by batching
+ * the vertices, colors, textures to the shaders using VAOs and VBOs.
+ * The Batcher is where, all the rendering takes place in SilenceEngine.
+ *
+ * The batcher is just instantiated like an object, usually only once
+ * in a game. Then, you can use the methods defined to add various
+ * shapes, or even raw vertices. They get rendered whenever the end
+ * method is called.
+ *
+ * A batcher can also take transforms, and Cameras and render using
+ * them. To use them, either pass them to the begin method, or apply
+ * them manually using the apply[Camera/Transform] methods.
+ *
+ * After the game completes, before the termination, the dispose method
+ * should be called by the user. Better place it in the dispose method
+ * of the game.
+ *
+ * @author Sri Harsha Chilakapati
+ */
+public class Batcher
+{
+    // Active state of this batcher
+    private boolean active = false;
+
+    // The sizes (no. of components) in vertex, color, texcoord
+    public static final int SIZE_OF_VERTEX = 4;
+    public static final int SIZE_OF_COLOR  = 4;
+    public static final int SIZE_OF_TEXCOORD = 2;
+
+    // The maximum number of vertices in a batch
+    public static final int MAX_VERTICES_IN_BATCH = 4096;
+
+    // The buffers to store the collected data
+    private FloatBuffer vBuffer;
+    private FloatBuffer cBuffer;
+    private FloatBuffer tBuffer;
+
+    // VAO and VBO handles
+    private int vaoID;
+    private int vboVertID;
+    private int vboColID;
+    private int vboTexID;
+
+    // The no. of vertices in the current batch
+    private int vertexCount;
+
+    // The transform, and projection and view matrices
+    private Transform transform;
+    private Matrix4   camProj;
+    private Matrix4   camView;
+
+    /**
+     * Creates the Batcher, and initialises OpenGL
+     */
+    public Batcher()
+    {
+        // Create the buffers
+        vBuffer = BufferUtils.createFloatBuffer(SIZE_OF_VERTEX   * MAX_VERTICES_IN_BATCH);
+        cBuffer = BufferUtils.createFloatBuffer(SIZE_OF_COLOR    * MAX_VERTICES_IN_BATCH);
+        tBuffer = BufferUtils.createFloatBuffer(SIZE_OF_TEXCOORD * MAX_VERTICES_IN_BATCH);
+
+        // Create the transformations
+        transform = new Transform();
+        camProj   = new Matrix4();
+        camView   = new Matrix4();
+
+        // Initialise OpenGL handles
+        initGLHandles();
+    }
+
+    /**
+     * Initialises VAOs and VBOs and creates the data store to
+     * store the entire batch.
+     */
+    private void initGLHandles()
+    {
+        // Create a VAO
+        vaoID = glGenVertexArrays();
+        glBindVertexArray(vaoID);
+
+        // Create VBOs
+        vboVertID = glGenBuffers();
+        vboColID = glGenBuffers();
+        vboTexID = glGenBuffers();
+
+        // Create vertex-buffer
+        glBindBuffer(GL_ARRAY_BUFFER, vboVertID);
+        glBufferData(GL_ARRAY_BUFFER, SIZE_OF_VERTEX * MAX_VERTICES_IN_BATCH, GL_STREAM_DRAW);
+
+        // Create color-buffer
+        glBindBuffer(GL_ARRAY_BUFFER, vboColID);
+        glBufferData(GL_ARRAY_BUFFER, SIZE_OF_COLOR * MAX_VERTICES_IN_BATCH, GL_STREAM_DRAW);
+
+        // Create texcoord-buffer
+        glBindBuffer(GL_ARRAY_BUFFER, vboTexID);
+        glBufferData(GL_ARRAY_BUFFER, SIZE_OF_TEXCOORD * MAX_VERTICES_IN_BATCH, GL_STREAM_DRAW);
+    }
+
+    /**
+     * Uploads the data collected to the OpenGL buffers.
+     */
+    private void uploadData()
+    {
+        // Upload vertices
+        glBindBuffer(GL_ARRAY_BUFFER, vboVertID);
+        glBufferSubData(GL_ARRAY_BUFFER, 0, vBuffer);
+
+        glVertexAttribPointer(0, 4, GL_FLOAT, false, 0, 0);
+
+        // Upload colors
+        glBindBuffer(GL_ARRAY_BUFFER, vboColID);
+        glBufferSubData(GL_ARRAY_BUFFER, 0, cBuffer);
+
+        glVertexAttribPointer(1, 4, GL_FLOAT, false, 0, 0);
+
+        // Upload tex-coords
+        glBindBuffer(GL_ARRAY_BUFFER, vboTexID);
+        glBufferSubData(GL_ARRAY_BUFFER, 0, tBuffer);
+
+        glVertexAttribPointer(2, 2, GL_FLOAT, false, 0, 0);
+    }
+
+    /**
+     * Begins the batcher with a Transform. All the vertices
+     * collected will get applied with that Transform
+     *
+     * @param t The transform object to do transformation
+     */
+    public void begin(Transform t)
+    {
+        applyTransform(t);
+        begin();
+    }
+
+    /**
+     * Begins the batcher with a Camera.
+     * @param c The camera to use.
+     */
+    public void begin(ICamera c)
+    {
+        applyCamera(c);
+        begin();
+    }
+
+    /**
+     * Begins the batcher with a Camera and a Transform
+     * @param c The camera to use.
+     * @param t The transform object to do transformation
+     */
+    public void begin(ICamera c, Transform t)
+    {
+        applyCamera(c);
+        applyTransform(t);
+        begin();
+    }
+
+    /**
+     * Begins the batcher and marks it active.
+     */
+    public void begin()
+    {
+        if (active)
+            throw new IllegalStateException("Batcher Already Active!");
+
+        active = true;
+        vertexCount = 0;
+    }
+
+    /**
+     * Ends the batcher by flushing all the data to the OpenGL
+     */
+    public void end()
+    {
+        if (!active)
+            throw new IllegalStateException("Batcher not Active!");
+
+        active = false;
+        flush();
+
+        // Reset Transform and camera matrices
+        transform.reset();
+        camProj.initIdentity();
+        camView.initIdentity();
+    }
+
+    /**
+     * Flushes the data collected to the GPU.
+     */
+    public void flush()
+    {
+        // Avoid doing unnecessary flushes
+        if (vertexCount == 0)
+            return;
+
+        // Shader uniforms
+        Shader.CURRENT.setUniform("tex", Texture.getActiveUnit());
+        Shader.CURRENT.setUniform("mTransform", transform.getMatrix());
+        Shader.CURRENT.setUniform("camProj", camProj);
+        Shader.CURRENT.setUniform("camView", camView);
+
+        // Flip the buffers
+        vBuffer.flip();
+        cBuffer.flip();
+        tBuffer.flip();
+
+        // Bind the VAO
+        glBindVertexArray(vaoID);
+        glEnableVertexAttribArray(0);
+        glEnableVertexAttribArray(1);
+        glEnableVertexAttribArray(2);
+
+        // Upload the data
+        uploadData();
+
+        // Do a rendering
+        glDrawArrays(GL_TRIANGLES, 0, vertexCount);
+
+        // Unbind the VAO
+        glDisableVertexAttribArray(0);
+        glDisableVertexAttribArray(1);
+        glDisableVertexAttribArray(2);
+        glBindVertexArray(0);
+
+        // Clear the buffers
+        vBuffer.clear();
+        cBuffer.clear();
+        tBuffer.clear();
+
+        // Clear the vertex count
+        vertexCount = 0;
+    }
+
+    /**
+     *
+     * @param t
+     */
+    public void applyTransform(Transform t)
+    {
+        // Flush the data first
+        flush();
+
+        // Apply transform
+        transform.apply(t);
+    }
+
+    public void applyCamera(ICamera c)
+    {
+        // Flush the data first
+        flush();
+
+        camProj.initIdentity().multiply(c.getProjection());
+        camView.initIdentity().multiply(c.getView());
+    }
+
+    /* addVertex variations */
+
+    public void addVertex(Vector2 v)
+    {
+        addVertex(v.getX(), v.getY(), 0, 1);
+    }
+
+    public void addVertex(Vector2 v, Vector2 t)
+    {
+        addVertex(v.getX(), v.getY(), 0, 1, 0, 0, 0, 1, t.getX(), t.getY());
+    }
+
+    public void addVertex(Vector2 v, Color c)
+    {
+        addVertex(v.getX(), v.getY(), 0, 1, c.getR(), c.getG(), c.getB(), c.getA());
+    }
+
+    public void addVertex(Vector2 v, Color c, Vector2 t)
+    {
+        addVertex(v.getX(), v.getY(), 0, 1, c.getR(), c.getG(), c.getB(), c.getA(), t.getX(), t.getY());
+    }
+
+    public void addVertex(Vector3 v)
+    {
+        addVertex(v.getX(), v.getY(), v.getZ(), 1);
+    }
+
+    public void addVertex(Vector3 v, Vector2 t)
+    {
+        addVertex(v.getX(), v.getY(), v.getZ(), 1, 0, 0, 0, 1, t.getX(), t.getY());
+    }
+
+    public void addVertex(Vector3 v, Color c)
+    {
+        addVertex(v.getX(), v.getY(), v.getZ(), 1, c.getR(), c.getG(), c.getB(), c.getA());
+    }
+
+    public void addVertex(Vector3 v, Color c, Vector2 t)
+    {
+        addVertex(v.getX(), v.getY(), v.getZ(), 1, c.getR(), c.getG(), c.getB(), c.getA(), t.getX(), t.getY());
+    }
+
+    public void addVertex(Vector4 v)
+    {
+        addVertex(v.getX(), v.getY(), v.getZ(), v.getW());
+    }
+
+    public void addVertex(Vector4 v, Vector2 t)
+    {
+        addVertex(v.getX(), v.getY(), v.getZ(), v.getW(), 0, 0, 0, 1, t.getX(), t.getY());
+    }
+
+    public void addVertex(Vector4 v, Color c)
+    {
+        addVertex(v.getX(), v.getY(), v.getZ(), v.getW(), c.getR(), c.getG(), c.getB(), c.getA());
+    }
+
+    public void addVertex(Vector4 v, Color c, Vector2 t)
+    {
+        addVertex(v.getX(), v.getY(), v.getZ(), v.getW(), c.getR(), c.getG(), c.getB(), c.getA(), t.getX(), t.getY());
+    }
+
+    public void addVertex(float x, float y, float z, float w)
+    {
+        addVertex(x, y, z, w, 1, 1, 1, 1, 0, 0);
+    }
+
+    public void addVertex(float x, float y, float z, float w, float u, float v)
+    {
+        addVertex(x, y, z, w, 0, 0, 0, 0, u, v);
+    }
+
+    public void addVertex(float x, float y, float z, float w, float r, float g, float b, float a)
+    {
+        addVertex(x, y, z, w, r, g, b, a, 0, 0);
+    }
+
+    public void addVertex(float x, float y, float z, float w, float r, float g, float b, float a, float u, float v)
+    {
+        flushOnOverflow(1);
+
+        vertexCount++;
+        vBuffer.put(x).put(y).put(z).put(w);
+        cBuffer.put(r).put(g).put(b).put(a);
+        tBuffer.put(u).put(v);
+    }
+
+    /* addTriangle variations */
+
+    public void addTriangle(Vector2 v1, Vector2 v2, Vector2 v3,
+                            Color   c1, Color   c2, Color   c3,
+                            Vector2 t1, Vector2 t2, Vector2 t3)
+    {
+        addTriangle(v1.getX(), v1.getY(), 0, 1,
+                    v2.getX(), v2.getY(), 0, 1,
+                    v3.getX(), v3.getY(), 0, 1,
+                    c1.getR(), c1.getG(), c1.getB(), c1.getA(),
+                    c2.getR(), c2.getG(), c2.getB(), c2.getA(),
+                    c3.getR(), c3.getG(), c3.getB(), c3.getA(),
+                    t1.getX(), t1.getY(),
+                    t2.getX(), t2.getY(),
+                    t3.getX(), t3.getY());
+    }
+
+    public void addTriangle(Vector2 v1, Vector2 v2, Vector2 v3)
+    {
+        addTriangle(v1, v2, v3, Color.WHITE, Color.WHITE, Color.WHITE, Vector2.ZERO, Vector2.ZERO, Vector2.ZERO);
+    }
+
+    public void addTriangle(Vector2 v1, Vector2 v2, Vector2 v3,
+                            Color   c1, Color   c2, Color   c3)
+    {
+        addTriangle(v1, v2, v3, c1, c2, c3, Vector2.ZERO, Vector2.ZERO, Vector2.ZERO);
+    }
+
+    public void addTriangle(Vector2 v1, Vector2 v2, Vector2 v3,
+                            Vector2 t1, Vector2 t2, Vector2 t3)
+    {
+        addTriangle(v1, v2, v3, Color.TRANSPARENT, Color.TRANSPARENT, Color.TRANSPARENT, t1, t2, t3);
+    }
+
+    public void addTriangle(Vector3 v1, Vector3 v2, Vector3 v3,
+                            Color   c1, Color   c2, Color   c3,
+                            Vector2 t1, Vector2 t2, Vector2 t3)
+    {
+        addTriangle(v1.getX(), v1.getY(), v1.getZ(), 1,
+                    v2.getX(), v2.getY(), v2.getZ(), 1,
+                    v3.getX(), v3.getY(), v3.getZ(), 1,
+                    c1.getR(), c1.getG(), c1.getB(), c1.getA(),
+                    c2.getR(), c2.getG(), c2.getB(), c2.getA(),
+                    c3.getR(), c3.getG(), c3.getB(), c3.getA(),
+                    t1.getX(), t1.getY(), t2.getX(), t2.getY(), t3.getX(), t3.getY());
+    }
+
+    public void addTriangle(float x1, float y1, float z1, float w1,
+                            float x2, float y2, float z2, float w2,
+                            float x3, float y3, float z3, float w3,
+                            float r1, float g1, float b1, float a1,
+                            float r2, float g2, float b2, float a2,
+                            float r3, float g3, float b3, float a3,
+                            float u1, float v1,
+                            float u2, float v2,
+                            float u3, float v3)
+    {
+        flushOnOverflow(3);
+
+        addVertex(x1, y1, z1, w1, r1, g1, b1, a1, u1, v1);
+        addVertex(x2, y2, z2, w2, r2, g2, b2, a2, u2, v2);
+        addVertex(x3, y3, z3, w3, r3, g3, b3, a3, u3, v3);
+    }
+
+    public void flushOnOverflow(int capacity)
+    {
+        if (vertexCount + capacity > MAX_VERTICES_IN_BATCH)
+            flush();
+    }
+
+    public void dispose()
+    {
+        glBindVertexArray(0);
+        glDeleteVertexArrays(vaoID);
+        glBindBuffer(GL_ARRAY_BUFFER, 0);
+        glDeleteBuffers(vboVertID);
+        glDeleteBuffers(vboColID);
+    }
+}
