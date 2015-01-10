4c33d852f560257c8c9767b6aaa2b7b30bd305e1
Sri Harsha Chilakapati <sriharshachilakapati@gmail.com>
2015-01-10 16:14:24 +0530

    Fixing a bug causing self collisions with QuadTrees

--- src/main/java/com/shc/silenceengine/collision/colliders/QuadTreeSceneCollider.java
+++ src/main/java/com/shc/silenceengine/collision/colliders/QuadTreeSceneCollider.java
@@ -90,9 +90,10 @@ public class QuadTreeSceneCollider implements SceneCollider2D
 
                     for (Entity2D entity2 : collidables)
                         if (collisionMap.get(class1).isInstance(entity2))
-                            // Check collision
-                            if (entity.getPolygon().intersects(entity2.getPolygon()))
-                                entity.collision(entity2);
+                            if (entity != entity2)
+                                // Check collision
+                                if (entity.getPolygon().intersects(entity2.getPolygon()))
+                                    entity.collision(entity2);
                 }
     }
 }
