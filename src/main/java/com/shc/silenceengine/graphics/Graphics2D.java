662ea780272f3471d03a79b58897fc0140535885
Sri Harsha Chilakapati <sriharshachilakapati@gmail.com>
2015-01-31 16:30:07 +0530

    Restructured the Graphics2D class

--- src/main/java/com/shc/silenceengine/graphics/Graphics2D.java
+++ src/main/java/com/shc/silenceengine/graphics/Graphics2D.java
@@ -45,33 +45,55 @@ public class Graphics2D
     }
 
     /* SHAPE DRAWING METHODS */
+    private void rect(float x, float y, float w, float h, Primitive primitive)
+    {
+        startPainting();
+        {
+            Batcher batcher = Game.getBatcher();
+
+            batcher.applyTransform(transform);
+            batcher.begin(primitive);
+            {
+                batcher.vertex(x, y);
+                batcher.color(color);
+
+                batcher.vertex(x + w, y);
+                batcher.color(color);
+
+                batcher.vertex(x + w, y + h);
+                batcher.color(color);
+
+                batcher.vertex(x, y + h);
+                batcher.color(color);
+            }
+            batcher.end();
+        }
+        endPainting();
+    }
+
+    private void oval(float x, float y, float rx, float ry, Primitive primitive)
+    {
+        startPainting();
+        {
+            Batcher batcher = Game.getBatcher();
+
+            batcher.applyTransform(transform);
+            batcher.begin(primitive);
+            {
+                for (int i = 0; i < 360; i++)
+                {
+                    batcher.vertex(x + MathUtils.cos(i) * rx, y + MathUtils.sin(i) * ry);
+                    batcher.color(color);
+                }
+            }
+            batcher.end();
+        }
+        endPainting();
+    }
 
     public void drawRect(float x, float y, float w, float h)
     {
-        BaseCamera originalCam = BaseCamera.CURRENT;
-        camera.apply();
-
-        Batcher batcher = Game.getBatcher();
-
-        batcher.applyTransform(transform);
-        batcher.begin(Primitive.LINE_LOOP);
-        {
-            batcher.vertex(x, y);
-            batcher.color(color);
-
-            batcher.vertex(x + w, y);
-            batcher.color(color);
-
-            batcher.vertex(x + w, y + h);
-            batcher.color(color);
-
-            batcher.vertex(x, y + h);
-            batcher.color(color);
-        }
-        batcher.end();
-
-        if (originalCam != null)
-            originalCam.apply();
+        rect(x, y, w, h, Primitive.LINE_LOOP);
     }
 
     public void drawRect(Vector2 pos, float w, float h)
@@ -82,35 +104,12 @@ public class Graphics2D
     public void drawRect(Vector2 min, Vector2 max)
     {
         Vector2 size = max.subtract(min);
-        drawRect(min.x, max.x, size.x, size.y);
+        drawRect(min.x, min.y, size.x, size.y);
     }
 
     public void fillRect(float x, float y, float w, float h)
     {
-        BaseCamera originalCam = BaseCamera.CURRENT;
-        camera.apply();
-
-        Batcher batcher = Game.getBatcher();
-
-        batcher.applyTransform(transform);
-        batcher.begin(Primitive.TRIANGLE_FAN);
-        {
-            batcher.vertex(x, y);
-            batcher.color(color);
-
-            batcher.vertex(x + w, y);
-            batcher.color(color);
-
-            batcher.vertex(x + w, y + h);
-            batcher.color(color);
-
-            batcher.vertex(x, y + h);
-            batcher.color(color);
-        }
-        batcher.end();
-
-        if (originalCam != null)
-            originalCam.apply();
+        rect(x, y, w, h, Primitive.TRIANGLE_FAN);
     }
 
     public void fillRect(Vector2 pos, float w, float h)
@@ -126,24 +125,7 @@ public class Graphics2D
 
     public void drawOval(float x, float y, float rx, float ry)
     {
-        BaseCamera originalCam = BaseCamera.CURRENT;
-        camera.apply();
-
-        Batcher batcher = Game.getBatcher();
-
-        batcher.applyTransform(transform);
-        batcher.begin(Primitive.LINE_LOOP);
-        {
-            for (int i = 0; i < 360; i++)
-            {
-                batcher.vertex(x + MathUtils.cos(i) * rx, y + MathUtils.sin(i) * ry);
-                batcher.color(color);
-            }
-        }
-        batcher.end();
-
-        if (originalCam != null)
-            originalCam.apply();
+        oval(x, y, rx, ry, Primitive.LINE_LOOP);
     }
 
     public void drawOval(Vector2 pos, float rx, float ry)
@@ -153,24 +135,7 @@ public class Graphics2D
 
     public void fillOval(float x, float y, float rx, float ry)
     {
-        BaseCamera originalCam = BaseCamera.CURRENT;
-        camera.apply();
-
-        Batcher batcher = Game.getBatcher();
-
-        batcher.applyTransform(transform);
-        batcher.begin(Primitive.TRIANGLE_FAN);
-        {
-            for (int i = 0; i < 360; i++)
-            {
-                batcher.vertex(x + MathUtils.cos(i) * rx, y + MathUtils.sin(i) * ry);
-                batcher.color(color);
-            }
-        }
-        batcher.end();
-
-        if (originalCam != null)
-            originalCam.apply();
+        oval(x, y, rx, ry, Primitive.TRIANGLE_FAN);
     }
 
     public void fillOval(Vector2 pos, float rx, float ry)
@@ -198,69 +163,80 @@ public class Graphics2D
         fillCircle(pos.x, pos.y, r);
     }
 
+    public void drawLine(float x1, float y1, float x2, float y2)
+    {
+        startPainting();
+        {
+            Batcher batcher = Game.getBatcher();
+
+            batcher.applyTransform(transform);
+            batcher.begin(Primitive.LINES);
+            {
+                batcher.vertex(x1, y1);
+                batcher.color(color);
+
+                batcher.vertex(x2, y2);
+                batcher.color(color);
+            }
+            batcher.end();
+        }
+        endPainting();
+    }
+
     public void drawPolygon(Polygon polygon)
     {
-        BaseCamera originalCam = BaseCamera.CURRENT;
-        camera.apply();
-
-        Batcher batcher = Game.getBatcher();
-        batcher.applyTransform(transform);
-        RenderUtils.tracePolygon(batcher, polygon, color);
-
-        if (originalCam != null)
-            originalCam.apply();
+        startPainting();
+        {
+            Batcher batcher = Game.getBatcher();
+            batcher.applyTransform(transform);
+            RenderUtils.tracePolygon(batcher, polygon, color);
+        }
+        endPainting();
     }
 
     public void fillPolygon(Polygon polygon)
     {
-        BaseCamera originalCam = BaseCamera.CURRENT;
-        camera.apply();
-
-        Batcher batcher = Game.getBatcher();
-        batcher.applyTransform(transform);
-        RenderUtils.fillPolygon(batcher, polygon, color);
-
-        if (originalCam != null)
-            originalCam.apply();
+        startPainting();
+        {
+            Batcher batcher = Game.getBatcher();
+            batcher.applyTransform(transform);
+            RenderUtils.fillPolygon(batcher, polygon, color);
+        }
+        endPainting();
     }
 
     /* TEXTURE DRAWING METHODS */
 
     public void drawTexture(Texture texture, float x, float y, float w, float h, boolean flipX, boolean flipY)
     {
-        BaseCamera originalCam = BaseCamera.CURRENT;
-        camera.apply();
-
-        Batcher batcher = Game.getBatcher();
-
-        Texture originalTexture = Texture.CURRENT;
-        texture.bind();
-
-        float minU = texture.getMinU();
-        float minV = texture.getMinV();
-        float maxU = texture.getMaxU();
-        float maxV = texture.getMaxV();
-
-        batcher.applyTransform(transform);
-        batcher.begin(Primitive.TRIANGLE_FAN);
+        startPainting();
         {
-            batcher.vertex(x, y);
-            batcher.texCoord(flipX ? maxU : minU, flipY ? maxV : minV);
+            Batcher batcher = Game.getBatcher();
+            texture.bind();
 
-            batcher.vertex(x + w, y);
-            batcher.texCoord(flipX ? minU : maxU, flipY ? maxV : minV);
+            float minU = texture.getMinU();
+            float minV = texture.getMinV();
+            float maxU = texture.getMaxU();
+            float maxV = texture.getMaxV();
 
-            batcher.vertex(x + w, y + h);
-            batcher.texCoord(flipX ? minU : maxU, flipY ? minV : maxV);
+            batcher.applyTransform(transform);
+            batcher.begin(Primitive.TRIANGLE_FAN);
+            {
+                batcher.vertex(x, y);
+                batcher.texCoord(flipX ? maxU : minU, flipY ? maxV : minV);
 
-            batcher.vertex(x, y + h);
-            batcher.texCoord(flipX ? maxU : minU, flipY ? minV : maxV);
+                batcher.vertex(x + w, y);
+                batcher.texCoord(flipX ? minU : maxU, flipY ? maxV : minV);
+
+                batcher.vertex(x + w, y + h);
+                batcher.texCoord(flipX ? minU : maxU, flipY ? minV : maxV);
+
+                batcher.vertex(x, y + h);
+                batcher.texCoord(flipX ? maxU : minU, flipY ? minV : maxV);
+            }
+            batcher.end();
         }
-        batcher.end();
-
-        originalTexture.bind();
-        if (originalCam != null)
-            originalCam.apply();
+        endPainting();
     }
 
     public void drawTexture(Texture texture, float x, float y)
@@ -294,15 +270,14 @@ public class Graphics2D
      */
     public void drawString(String string, float x, float y)
     {
-        BaseCamera originalCamera = BaseCamera.CURRENT;
-        camera.apply();
+        startPainting();
+        {
+            Batcher batcher = Game.getBatcher();
+            batcher.applyTransform(transform);
 
-        Batcher batcher = Game.getBatcher();
-        batcher.applyTransform(transform);
-
-        font.drawString(batcher, string, x, y, color);
-        if (originalCamera != null)
-            originalCamera.apply();
+            font.drawString(batcher, string, x, y, color);
+        }
+        endPainting();
     }
 
     public void drawString(String string, Vector2 pos)
@@ -341,6 +316,26 @@ public class Graphics2D
     public void resetTransform()
     {
         transform.reset();
+    }
+
+    /* Utility Methods */
+    private BaseCamera originalCamera;
+    private Texture originalTexture;
+
+    protected void startPainting()
+    {
+        originalCamera = BaseCamera.CURRENT;
+        camera.apply();
+
+        originalTexture = Texture.CURRENT;
+    }
+
+    protected void endPainting()
+    {
+        originalTexture.bind();
+
+        if (originalCamera != null)
+            originalCamera.apply();
     }
 
     /**
