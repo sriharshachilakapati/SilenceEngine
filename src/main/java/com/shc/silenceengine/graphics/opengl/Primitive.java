e83eeef8281a500410522a89136a73f0e9c26c8b
Gamefreak0 <gamefreak10000@gmail.com>
2015-03-29 08:49:31 -0400

    Documenting GL3Context & Primitive

--- src/main/java/com/shc/silenceengine/graphics/opengl/Primitive.java
+++ src/main/java/com/shc/silenceengine/graphics/opengl/Primitive.java
@@ -27,6 +27,9 @@ package com.shc.silenceengine.graphics.opengl;
 import static org.lwjgl.opengl.GL11.*;
 
 /**
+ * Primitives are used for rendering VertexArrays or other objects to the screen. The most common is TRIANGLES. For
+ * more information read <a href="https://github.com/sriharshachilakapati/SilenceEngine/wiki/Working-with-the-Batcher#the-primitive">here</a>.
+ *
  * @author Sri Harsha Chilakapati
  */
 public enum Primitive
