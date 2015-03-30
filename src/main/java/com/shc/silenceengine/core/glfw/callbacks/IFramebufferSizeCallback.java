e3cc824f3cfb7494f70c3fa01dc76c180c9d17c8
Sri Harsha Chilakapati <hello@goharsha.com>
2015-03-30 22:58:35 +0530

    Updated IFramebufferSizeCallback

--- src/main/java/com/shc/silenceengine/core/glfw/callbacks/IFramebufferSizeCallback.java
+++ src/main/java/com/shc/silenceengine/core/glfw/callbacks/IFramebufferSizeCallback.java
@@ -32,5 +32,5 @@ import com.shc.silenceengine.core.glfw.Window;
 @FunctionalInterface
 public interface IFramebufferSizeCallback
 {
-    public void invoke(Window window, int width, int height);
+    void invoke(Window window, int width, int height);
 }
