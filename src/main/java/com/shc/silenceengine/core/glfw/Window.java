f6080563c7a534df70ab359455816066421f3cde
Sri Harsha Chilakapati <hello@goharsha.com>
2015-05-03 06:16:41 +0530

    Partial documentation of the Window class

--- src/main/java/com/shc/silenceengine/core/glfw/Window.java
+++ src/main/java/com/shc/silenceengine/core/glfw/Window.java
@@ -42,10 +42,73 @@ import static org.lwjgl.glfw.GLFW.*;
 import static org.lwjgl.system.MemoryUtil.*;
 
 /**
+ * <p> This class is an object oriented wrapper for GLFWwindow structure, which encapsulates both the window and the
+ * context. For example, this creates a 640 by 480 windowed mode window.</p>
+ *
+ * <pre>
+ *     Window window = new Window(640, 480, "My Title");
+ * </pre>
+ *
+ * <p> There are several other overloads of the constructors allowing you to specify the size, title, monitor, a parent
+ * window and the video mode. The window object is provided along with the input events, so the event handlers can tell
+ * which window recieved the input.</p>
+ *
+ * <h2>Full screen windows</h2>
+ *
+ * <p> To create a full screen window, you need to specify which monitor the window should use. In most cases, the
+ * user's primary monitor is a good choice. For more information about retrieving monitors, see
+ * {@link Monitor#getMonitors()}.</p>
+ *
+ * <pre>
+ *     Window window = new Window(640, 480, Monitor.getPrimaryMonitor());
+ * </pre>
+ *
+ * <p> Full screen windows cover the entire display area of a monitor, have no border or decorations. You can also pass
+ * a {@link VideoMode} to the constructor along with the monitor to change the resolution of the monitor.</p>
+ *
+ * <h2>Window hints</h2>
+ *
+ * <p> There are a number of hints that can be set before the creation of a window and context. Some affect the window
+ * itself, others affect the framebuffer or context. These hints are set to their default values each time the library
+ * is initialized with {@link GLFW3#init()}, can be set individually with {@link Window#setHint(int, boolean)} or
+ * {@link Window#setHint(int, int)}, and reset all at once to their defaults with {@link Window#setDefaultHints()}.</p>
+ *
+ * <pre>
+ *     Window.setHint(GLFW.GLFW_CONTEXT_VERSION_MAJOR, 3);
+ *     Window.setHint(GLFW.GLFW_CONTEXT_VERSION_MINOR, 2);
+ *     Window.setHint(GLFW.GLFW_OPENGL_FORWARD_COMPAT, true);
+ *     Window.setHint(GLFW.GLFW_OPENGL_PROFILE, GLFW.GLFW_OPENGL_CORE_PROFILE);
+ *
+ *     Window coreProfileWindow = new Window(640, 480);
+ * </pre>
+ *
+ * <p> Note that hints need to be set before the creation of the window and context you wish to have the specified
+ * attributes.</p>
+ *
+ * <h2>Context object sharing</h2>
+ *
+ * <p> When creating a window and its OpenGL context, you can specify another window whose context the new one should
+ * share its objects (textures, vertex and element buffers, etc.) with.</p>
+ *
+ * <pre>
+ *     Window secondWindow = new Window(640, 480, "Second Window", firstWindow);
+ * </pre>
+ *
+ * <p> Object sharing is implemented by the operating system and graphics driver. On platforms where it is possible to
+ * choose which types of objects are shared, GLFW requests that all types are shared. A common exception to this is VAO
+ * objects, they have to be recreated for every context.</p>
+ *
+ * @see Monitor
+ * @see VideoMode
+ * @see Cursor
+ *
  * @author Sri Harsha Chilakapati
  */
 public class Window
 {
+    // A HashMap to store the Window objects based on the handle of the window.
+    // This is used to send the Window object to the input events because, say
+    // long pointers are ugly in Java.
     private static Map<Long, Window> registeredWindows = new HashMap<>();
 
     private long handle;
@@ -90,21 +153,76 @@ public class Window
     private IWindowRefreshCallback   windowRefreshCallback;
     private IWindowSizeCallback      windowSizeCallback;
 
+    /**
+     * Creates a window which is 800 by 600 in dimensions with the title "SilenceEngine Window".
+     */
     public Window()
     {
         this(800, 600);
     }
 
+    /**
+     * Creates a windowed mode window with a specified width and height, in screen coordinates. The title will be the
+     * default title which is "SilenceEngine Window".
+     *
+     * @param width  The desired width, in screen coordinates, of the window. This must be greater than zero.
+     * @param height The desired height, in screen coordinates, of the window. This must be greater than zero.
+     */
     public Window(int width, int height)
     {
         this(width, height, (Monitor) null, null);
     }
 
+
+    /**
+     * Crates a window and its associated OpenGL context, by taking into account the resolution of the screen and the
+     * monitor that the window should be made full screen on, and another window if you want to share the context of the
+     * other window.
+     *
+     * @param width   The desired width, in screen coordinates, of the window. This must be greater than zero.
+     * @param height  The desired height, in screen coordinates, of the window. This must be greater than zero.
+     * @param monitor The monitor to use for full screen mode, or <code>null</code> to use windowed mode.
+     * @param share   The window whose context to share resources with, or <code>null</code> to not share resources.
+     */
     public Window(int width, int height, Monitor monitor, Window share)
     {
         this(width, height, "SilenceEngine Window", monitor, share);
     }
 
+    /**
+     * <p> Creates a window and its associated OpenGL context. Most of the options controlling how the window and its
+     * context should be created are specified with window hints.</p>
+     *
+     * <p> Successful creation does not change which context is current. Before you can use the newly created context,
+     * you need to make it current.</p>
+     *
+     * <p> The created window, framebuffer and context may differ from what you requested, as not all parameters and
+     * hints are hard constraints. This includes the size of the window, especially for full screen windows. You could
+     * query them manually after the window is created.</p>
+     *
+     * <p> To create a full screen window, you need to specify the monitor the window will cover. If no monitor is
+     * specified, windowed mode will be used. Unless you have a way for the user to choose a specific monitor, it is
+     * recommended that you pick the primary monitor.</p>
+     *
+     * <p> For full screen windows, the specified size becomes the resolution of the window's desired video mode. As
+     * long as a full screen window has input focus, the supported video mode most closely matching the desired video
+     * mode is set for the specified monitor.</p>
+     *
+     * <p> By default, newly created windows use the placement recommended by the window system. To create the window at
+     * a specific position, make it initially invisible using the <code>GLFW_VISIBLE</code> window hint, set its
+     * position and then show it.</p>
+     *
+     * <p> If a full screen window has input focus, the screensaver is prohibited from starting.</p>
+     *
+     * <p> The swap interval is not set during window creation and the initial value may vary depending on driver
+     * settings and defaults.</p>
+     *
+     * @param width   The desired width, in screen coordinates, of the window. This must be greater than zero.
+     * @param height  The desired height, in screen coordinates, of the window. This must be greater than zero.
+     * @param title   The desired title string of the Window. This can also contain UTF-8 characters.
+     * @param monitor The monitor to use for full screen mode, or <code>null</code> to use windowed mode.
+     * @param share   The window whose context to share resources with, or <code>null</code> to not share resources.
+     */
     public Window(int width, int height, String title, Monitor monitor, Window share)
     {
         size = new Vector2(width, height);
@@ -123,11 +241,26 @@ public class Window
         initNativeCallbacks();
     }
 
+    /**
+     * This constructor creates a fullscreen mode window that fills the monitor. The resolution will be set to the default
+     * resolution of the monitor supplied. If the monitor is supplied as <code>null</code>, then a windowed mode window
+     * is created instead which is 800 by 600 in size.
+     *
+     * @param monitor The monitor to use for full screen mode, or <code>null</code> to use windowed mode.
+     */
     public Window(Monitor monitor)
     {
-        this(800, 600, monitor);
+        this(monitor != null ? monitor.getVideoMode().getWidth() : 800,
+                monitor != null ? monitor.getVideoMode().getHeight() : 600, monitor);
     }
 
+    /**
+     * Creates a full screen window over a specified monitor. The width and height are the resolution of the monitor.
+     *
+     * @param width   The desired width, in screen coordinates, of the window. This must be greater than zero.
+     * @param height  The desired height, in screen coordinates, of the window. This must be greater than zero.
+     * @param monitor The monitor to use for full screen mode, or <code>null</code> to use windowed mode.
+     */
     public Window(int width, int height, Monitor monitor)
     {
         this(width, height, "SilenceEngine Window", monitor);
