d7f072ec82e2b16116466b295442c2dec144933f
Sri Harsha Chilakapati <hello@goharsha.com>
2015-06-14 07:57:38 +0530

    Modified GLFWTest to look like the multithreaded test from lwjgl3-demos

--- src/main/java/com/shc/silenceengine/tests/GLFWTest.java
+++ src/main/java/com/shc/silenceengine/tests/GLFWTest.java
@@ -24,198 +24,212 @@
 
 package com.shc.silenceengine.tests;
 
-import com.shc.silenceengine.core.SilenceEngine;
-import com.shc.silenceengine.core.glfw.Cursor;
+import com.shc.silenceengine.core.SilenceException;
 import com.shc.silenceengine.core.glfw.GLFW3;
+import com.shc.silenceengine.core.glfw.Monitor;
+import com.shc.silenceengine.core.glfw.VideoMode;
 import com.shc.silenceengine.core.glfw.Window;
 import com.shc.silenceengine.graphics.Batcher;
 import com.shc.silenceengine.graphics.Color;
+import com.shc.silenceengine.graphics.cameras.PerspCam;
 import com.shc.silenceengine.graphics.opengl.GL3Context;
 import com.shc.silenceengine.graphics.opengl.Program;
 import com.shc.silenceengine.graphics.opengl.Shader;
-import com.shc.silenceengine.graphics.opengl.Texture;
 import com.shc.silenceengine.input.Keyboard;
+import com.shc.silenceengine.math.Transform;
+import com.shc.silenceengine.math.Vector3;
 import com.shc.silenceengine.utils.NativesLoader;
-import com.shc.silenceengine.utils.TimeUtils;
 import org.lwjgl.glfw.GLFW;
 import org.lwjgl.opengl.GL11;
 import org.lwjgl.opengl.GL20;
 
 /**
- * This is the getting started example on the GLFW website ported to use the GLFW wrapper API.
+ * This is a minimal GLFW test that uses the GLFW wrapper classes and multi threading. This one is modified GLFW demo
+ * presented in the LWJGL3 demos repository written by Kai Burjack. Original code can be found at <a
+ * href="https://github.com/LWJGL/lwjgl3-demos/blob/master/src/org/lwjgl/demo/opengl/glfw/Multithreaded.java">
+ * https://github.com/LWJGL/lwjgl3-demos/blob/master/src/org/lwjgl/demo/opengl/glfw/Multithreaded.java</a>
  *
  * @author Sri Harsha Chilakapati
  */
 public class GLFWTest
 {
-    private static Batcher batcher;
+    private final Object lock = new Object();
+
+    private Window  window;
+    private boolean running;
+
+    private int width;
+    private int height;
+
+    private Program program;
 
     public static void main(String[] args)
     {
-        // Start with loading the natives
+        new GLFWTest().run();
+    }
+
+    private void run()
+    {
         NativesLoader.loadLWJGL();
 
-        // Initialize GLFW library
         if (!GLFW3.init())
-            return;
+            throw new SilenceException("Failed to initialize GLFW3");
 
+        // Set the window hints
+        Window.setDefaultHints();
+        Window.setHint(GLFW.GLFW_RESIZABLE, true);
+        Window.setHint(GLFW.GLFW_VISIBLE, false);
         Window.setHint(GLFW.GLFW_CONTEXT_VERSION_MAJOR, 3);
-        Window.setHint(GLFW.GLFW_CONTEXT_VERSION_MINOR, 2);
+        Window.setHint(GLFW.GLFW_CONTEXT_VERSION_MINOR, 3);
         Window.setHint(GLFW.GLFW_OPENGL_FORWARD_COMPAT, true);
         Window.setHint(GLFW.GLFW_OPENGL_PROFILE, GLFW.GLFW_OPENGL_CORE_PROFILE);
-        Window.setHint(GLFW.GLFW_SAMPLES, 4);
-
-        // Create a windowed mode window and its OpenGL context
-        Window window = new Window(640, 480, "Hello World");
-
-        // Make the window's context current
-        window.makeCurrent();
-
-        // Change cursor
-        Texture cursorImage = Texture.fromResource("resources/cursor.png");
-        Cursor cursor = new Cursor(cursorImage);
-
-        window.setCursor(cursor);
-
-        batcher = new Batcher();
-        batcher.setVertexLocation(0);
-        batcher.setColorLocation(1);
-        batcher.setNormalLocation(2);
-        batcher.setTexCoordLocation(3);
-
-        Shader vs = new Shader(GL20.GL_VERTEX_SHADER);
-        Shader fs = new Shader(GL20.GL_FRAGMENT_SHADER);
-
-        vs.source(
-                "#version 330 core                               \n" +
-
-                // Just declare others for the sake of Batcher
-                "layout (location = 0) in vec4 position;         \n" +
-                "layout (location = 1) in vec4 color;            \n" +
-                "layout (location = 2) in vec4 normal;           \n" +
-                "layout (location = 3) in vec2 texCoords;        \n" +
-
-                "out vec4 fsColor;                               \n" +
-
-                "void main()                                     \n" +
-                "{                                               \n" +
-                "    gl_Position = position;                     \n" +
-                "    fsColor = color;                            \n" +
-                "}"
-        );
-
-        fs.source(
-                "#version 330 core               \n" +
-
-                "in vec4 fsColor;                \n" +
-                "out vec4 fragColor;             \n" +
-
-                "void main()                     \n" +
-                "{                               \n" +
-                "    fragColor = fsColor;        \n" +
-                "}"
-        );
-
-        vs.compile();
-        fs.compile();
-
-        Program shaderProgram = new Program();
-
-        shaderProgram.attach(vs);
-        shaderProgram.attach(fs);
-        shaderProgram.link();
-
-        vs.dispose();
-        fs.dispose();
-
-        shaderProgram.use();
-
-        window.setRefreshCallback(GLFWTest::glfwRefreshCallback);
-        window.setSizeCallback(GLFWTest::glfwResizeCallback);
-
-        // Initialize the input engine
-        SilenceEngine.input.init();
 
         GLFW3.setSwapInterval(1);
 
-        int frames = 0;
-        int fps;
-        double start, end;
+        // Create the Window
+        window = new Window(width = 640, height = 480, "GLFW Test");
+        window.setKeyCallback(this::glfwKeyCallback);
+        window.setSizeCallback(this::glfwWindowSizeCallback);
 
-        start = TimeUtils.currentSeconds();
+        // Center the window on screen
+        VideoMode videoMode = Monitor.getPrimaryMonitor().getVideoMode();
+        window.setPosition((videoMode.getWidth() - 640) / 2, (videoMode.getHeight() - 480) / 2);
+        window.show();
 
-        // Loop until the user closes the window
+        // Start the rendering thread
+        new Thread(this::renderLoop).start();
+
+        // Start the event message loop
+        running = true;
         while (!window.shouldClose())
         {
-            // Tell the input engine to begin a new frame
-            SilenceEngine.input.beginFrame();
-
-            if (Keyboard.isClicked(Keyboard.KEY_ESCAPE))
-                break;
-
-            // Render here
-            render();
-
-            // Swap front and back buffers
-            window.swapBuffers();
-
-            // Poll for and process events
-            GLFW3.pollEvents();
-
-            // Tell the input engine to end the current frame
-            SilenceEngine.input.endFrame();
-
-            end = TimeUtils.currentSeconds();
-
-            frames++;
-
-            if (end - start > 1)
-            {
-                fps = frames;
-                frames = 0;
-                start = end;
-
-                window.setTitle("FPS: " + fps);
-            }
+            GLFW3.waitEvents();
         }
 
-        batcher.dispose();
-        shaderProgram.dispose();
-
-        window.destroy();
-        cursor.destroy();
+        // Destroy the window once we are no longer running
+        synchronized (lock)
+        {
+            running = false;
+            window.destroy();
+        }
 
         GLFW3.terminate();
     }
 
-    private static void glfwRefreshCallback(Window window)
+    private void renderLoop()
     {
-        render();
-        window.swapBuffers();
-    }
+        window.makeCurrent();
 
-    private static void glfwResizeCallback(Window window, double width, double height)
-    {
-        GL3Context.viewport(0, 0, (float) width, (float) height);
-        render();
-        window.swapBuffers();
-    }
+        Batcher batcher = new Batcher();
+        initShaders();
 
-    private static void render()
-    {
-        GL3Context.clear(GL11.GL_COLOR_BUFFER_BIT);
+        batcher.setVertexLocation(0);
+        batcher.setColorLocation(1);
+        batcher.setTexCoordLocation(2);
+        batcher.setNormalLocation(3);
 
-        batcher.begin();
+        PerspCam camera = new PerspCam();
+        Transform transform = new Transform();
+
+        while (running)
         {
-            batcher.vertex(+0.0f, +0.8f);
-            batcher.color(Color.RED);
+            // Apply the camera
+            camera.initProjection(70, ((float) width) / ((float) height), 0.1f, 100f);
+            camera.apply();
 
-            batcher.vertex(+0.8f, -0.8f);
-            batcher.color(Color.GREEN);
+            // Clear the context and set the viewport
+            GL3Context.clear(GL11.GL_COLOR_BUFFER_BIT | GL11.GL_DEPTH_BUFFER_BIT);
+            GL3Context.viewport(0, 0, width, height);
 
-            batcher.vertex(-0.8f, -0.8f);
-            batcher.color(Color.BLUE);
+            // Rotate the transform
+            transform.rotateSelf(Vector3.AXIS_Y, 1);
+
+            // Setup the uniforms for the batcher
+            program.setUniform("projection", camera.getProjection());
+            program.setUniform("view", camera.getView());
+            program.setUniform("transform", transform.getMatrix());
+
+            // Draw the triangle
+            batcher.begin();
+            {
+                batcher.vertex(+0.0f, +0.5f);
+                batcher.color(Color.RED);
+
+                batcher.vertex(+0.5f, -0.5f);
+                batcher.color(Color.GREEN);
+
+                batcher.vertex(-0.5f, -0.5f);
+                batcher.color(Color.BLUE);
+            }
+            batcher.end();
+
+            synchronized (lock)
+            {
+                if (running) window.swapBuffers();
+            }
         }
-        batcher.end();
+
+        batcher.dispose();
+        program.dispose();
+    }
+
+    private void initShaders()
+    {
+        Shader vs = new Shader(GL20.GL_VERTEX_SHADER);
+        vs.source(
+                "#version 330 core                                           \n" +
+
+                "layout (location = 0) in vec4 position;                     \n" +
+                "layout (location = 1) in vec4 color;                        \n" +
+                "layout (location = 2) in vec4 texCoords;                    \n" +
+                "layout (location = 3) in vec4 normal;                       \n" +
+
+                "out vec4 fsColor;" +
+
+                "uniform mat4 projection;                                    \n" +
+                "uniform mat4 view;                                          \n" +
+                "uniform mat4 transform;                                     \n" +
+
+                "void main()                                                 \n" +
+                "{                                                           \n" +
+                "    gl_Position = projection * view * transform * position; \n" +
+                "    fsColor = color;                                        \n" +
+                "}"
+        );
+        vs.compile();
+
+        Shader fs = new Shader(GL20.GL_FRAGMENT_SHADER);
+        fs.source(
+                "#version 330 core                                           \n" +
+
+                "in vec4 fsColor;                                            \n" +
+                "out vec4 fragColor;                                         \n" +
+
+                "void main()                                                 \n" +
+                "{                                                           \n" +
+                "    fragColor = fsColor;                                    \n" +
+                "}"
+        );
+        fs.compile();
+
+        program = new Program();
+        program.attach(vs);
+        program.attach(fs);
+        program.link();
+
+        vs.dispose();
+        fs.dispose();
+    }
+
+    private void glfwKeyCallback(Window window, int key, int scanCode, int action, int mods)
+    {
+        if (key == Keyboard.KEY_ESCAPE && action == GLFW.GLFW_PRESS)
+            window.setShouldClose(true);
+    }
+
+    private void glfwWindowSizeCallback(Window window, double width, double height)
+    {
+        this.width = (int) width;
+        this.height = (int) height;
     }
 }
