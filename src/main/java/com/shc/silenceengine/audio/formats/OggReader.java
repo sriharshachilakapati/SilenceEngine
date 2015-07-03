ee5c0ce4333a7f3645975d9b36080bdaa25725a4
Sri Harsha Chilakapati <hello@goharsha.com>
2015-07-03 11:38:32 +0530

    Commenting OggReader code

--- src/main/java/com/shc/silenceengine/audio/formats/OggReader.java
+++ src/main/java/com/shc/silenceengine/audio/formats/OggReader.java
@@ -42,8 +42,8 @@ import static org.lwjgl.system.MemoryUtil.*;
 
 /**
  * <p> A sound reader based on the STB vorbis libraries. That means, this class is able to read sound samples from a OGG
- * audio file in Vorbis encoding that the j-ogg and JOrbis libraries are able to decode. Currently, it can read from
- * .ogg and .oga file formats. </p>
+ * audio file in Vorbis encoding that the STB Vorbis class is able to decode. Currently, it can read from .ogg and .oga
+ * file formats. </p>
  *
  * <p> Though you are allowed to create instances of this reader on your own, it is recommended that you use the {@link
  * com.shc.silenceengine.audio.ISoundReader} interface to construct readers, as it is more flexible that way. And also,
@@ -83,11 +83,12 @@ public class OggReader implements ISoundReader
     }
 
     /**
-     * Borrowed from DecodeExample.java file of JOrbis examples, but heavily modified. This method reads in the OGG
-     * data, and convert it into PCM samples ready to be fed into the mouth of OpenAL ALBuffer class.
+     * This method reads in the OGG data, and convert it into PCM samples ready to be fed into the mouth of OpenAL
+     * ALBuffer class.
      */
     private void decodeToPCM(InputStream input)
     {
+        // Create a memory representation to read from memory
         ByteArrayOutputStream memory = new ByteArrayOutputStream();
         byte[] buffer = new byte[1024];
         int count = 0;
@@ -105,15 +106,18 @@ public class OggReader implements ISoundReader
             SilenceException.reThrow(e);
         }
 
+        // Copy the data to native memory
         ByteBuffer memoryBuffer = BufferUtils.createByteBuffer(memory.size());
         memoryBuffer.put(memory.toByteArray()).flip();
 
+        // Open the vorbis file to get stb_vorbis*
         IntBuffer error = BufferUtils.createIntBuffer(1);
         long handle = stb_vorbis_open_memory(memoryBuffer, error, null);
 
         if (handle == NULL)
             throw new SilenceException("Error " + error.get(0) + ": decoding the OGG data");
 
+        // Get the information about the OGG header
         STBVorbisInfo info = new STBVorbisInfo();
         stb_vorbis_get_info(handle, info.buffer());
 
@@ -122,10 +126,12 @@ public class OggReader implements ISoundReader
 
         format = channels == 1 ? ALFormat.MONO_16 : ALFormat.STEREO_16;
 
+        // Read all the samples once for all
         int numSamples = stb_vorbis_stream_length_in_samples(handle);
         ByteBuffer pcm = BufferUtils.createByteBuffer(numSamples * Short.BYTES);
         stb_vorbis_get_samples_short_interleaved(handle, channels, pcm, numSamples);
 
+        // Convert the audio bytes and store the data buffer
         data = ISoundReader.convertAudioBytes(pcm, channels == 2);
     }
 
