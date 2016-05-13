395d2f18a89759dae8e71551dbb77d2f2bd8b09b
Sri Harsha Chilakapati <sriharshachilakapati@gmail.com>
2016-05-13 11:13:52 +0530

    Remove pixel correction from sprite sheets

--- silenceengine/src/main/java/com/shc/silenceengine/graphics/SpriteSheet.java
+++ silenceengine/src/main/java/com/shc/silenceengine/graphics/SpriteSheet.java
@@ -58,10 +58,10 @@ public class SpriteSheet
 
         if (cells[column][row] == null)
         {
-            float minU = (column * cellWidth + 0.5f) / texture.getWidth();
-            float minV = (row * cellHeight + 0.5f) / texture.getHeight();
-            float maxU = ((column + 1) * cellWidth - 1.5f) / texture.getWidth();
-            float maxV = ((row + 1) * cellHeight - 1.5f) / texture.getHeight();
+            float minU = (column * cellWidth) / texture.getWidth();
+            float minV = (row * cellHeight) / texture.getHeight();
+            float maxU = ((column + 1) * cellWidth) / texture.getWidth();
+            float maxV = ((row + 1) * cellHeight) / texture.getHeight();
 
             cells[column][row] = texture.getSubTexture(minU, minV, maxU, maxV, cellWidth, cellHeight);
         }
